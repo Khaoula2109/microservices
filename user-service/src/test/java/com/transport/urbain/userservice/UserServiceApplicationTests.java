280d245de1f0a7467dea120d5bdbfe0ba3ca36af
OUAZRI Khaoula <ouazrikhaoula21@gmail.com>
2025-11-15 14:55:46 +0100

    Update test configuration for user-service

--- user-service/src/test/java/com/transport/urbain/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/transport/urbain/userservice/UserServiceApplicationTests.java
@@ -1,10 +1,13 @@
 package com.transport.urbain.userservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(excludeAutoConfiguration = RabbitAutoConfiguration.class)
+@SpringBootTest
+@TestPropertySource(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration"
+})
 class UserServiceApplicationTests {
 
     @Test
