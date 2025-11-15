cc2f1f57bc89f3e5655259189e6d908936a42743
OUAZRI Khaoula <ouazrikhaoula21@gmail.com>
2025-11-15 15:04:46 +0100

    Update test configuration for user-service

--- user-service/src/test/java/com/transport/urbain/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/transport/urbain/userservice/UserServiceApplicationTests.java
@@ -3,6 +3,7 @@ package com.transport.urbain.userservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
 @ActiveProfiles("test")
